6dcd624650e744280a6da0770dc180355f75eb9f
Michał Piekarski <michal.piekarski@icloud.com>
2014-07-24 00:18:18 +0200

    Łączenie z bibliotekami poprawione dla OSX.
    Zgodnie z nową strukturą folderów i sposobem w jakim są one umieszczone w repozytorium.

--- Brickpong/Brickpong/main.cpp
+++ Brickpong/Brickpong/main.cpp
@@ -8,8 +8,8 @@
 GLFWwindow* window;
 
     // Include GLM
-#include "glm.hpp"
-#include "gtc/matrix_transform.hpp"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 using namespace glm;
 
 #include "shader_loader.hpp"
