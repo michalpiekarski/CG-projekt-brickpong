2dd36c97bc993b9cc83af7a56dd151269495e1cc
Michał Piekarski <michal.piekarski@icloud.com>
2014-08-04 09:25:01 +0200

    Shadery zaktualizowane do nowej konwecji nazewnictwa (fs/vs).
    Wszelkie ustawienia projektu VS odpowiednio zaktualizowane.
    Wywołanie LoadShaders() w main.cpp zaktualizowane nowymi nazwami plików.

--- Brickpong/Brickpong/main.cpp
+++ Brickpong/Brickpong/main.cpp
@@ -272,7 +272,7 @@ int main( void ) {
 	glBindVertexArray(VertexArrayID);
 
         // Create and compile our GLSL program from the shaders
-	GLuint programID = LoadShaders( "TransformVertexShader.vertexshader", "ColorFragmentShader.fragmentshader" );
+	GLuint programID = LoadShaders( "TransformVertexShader.vs", "ColorFragmentShader.fs" );
 
         // Get a handle for our "MVP" uniform
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
