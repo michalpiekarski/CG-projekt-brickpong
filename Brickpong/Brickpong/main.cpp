b988fe778ad3b3f9e178d33b091acfabd18b9eaf
Michał Piekarski <michal.piekarski@icloud.com>
2014-08-04 09:39:11 +0200

    Kolejna poprawka konwencji nazwewnictwa shaderów.
    Rozszerzenia zmienione na vert/frag - odpowiednio poprawione parametry projektu VS oraz wywołanie LoadShaders() w main.cpp

--- Brickpong/Brickpong/main.cpp
+++ Brickpong/Brickpong/main.cpp
@@ -272,7 +272,7 @@ int main( void ) {
 	glBindVertexArray(VertexArrayID);
 
         // Create and compile our GLSL program from the shaders
-	GLuint programID = LoadShaders( "TransformVertexShader.vs", "ColorFragmentShader.fs" );
+	GLuint programID = LoadShaders( "shaders/TransformVertexShader.vert", "shaders/ColorFragmentShader.frag" );
 
         // Get a handle for our "MVP" uniform
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
