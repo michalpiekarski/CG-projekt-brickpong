2c708f4ab78c67ddfe7589ba2884bfdb39e770b6
Michał Piekarski <michal.piekarski@icloud.com>
2014-08-07 20:57:46 +0200

    Silnik fizyczny dodany w projekcie Xcode dla OS X.
    Ponad to w głównych nagłówkach silników graficznego i fizycznego poprawione ścieżki w poleceniach #include.

--- Brickpong/Brickpong/Graphics Engine/Graphics Engine.h
+++ Brickpong/Brickpong/Graphics Engine/Graphics Engine.h
@@ -1,11 +1,11 @@
 #ifndef __Brickpong__Graphics_Engine__
 #define __Brickpong__Graphics_Engine__
 
-#include "Graphics Engine/Window.h"
-#include "Graphics Engine/Shader.h"
-#include "Graphics Engine/ShaderProgram.h"
-#include "Graphics Engine/VAO.h"
-#include "Graphics Engine/VBO.h"
-#include "Graphics Engine/EBO.h"
+#include "Window.h"
+#include "Shader.h"
+#include "ShaderProgram.h"
+#include "VAO.h"
+#include "VBO.h"
+#include "EBO.h"
 
 #endif /* defined(__Brickpong__Graphics_Engine__) */
--- Brickpong/Brickpong/Physics Engine/Physics Engine.h
+++ Brickpong/Brickpong/Physics Engine/Physics Engine.h
@@ -1,12 +1,12 @@
 #ifndef __Brickpong__Physics_Engine__
 #define __Brickpong__Physics_Engine__
 
-#include "Physics Engine/Body.h"
-#include "Physics Engine/BodyAABB.h"
-#include "Physics Engine/BodyCircle.h"
-#include "Physics Engine/PhysicsMaterial.h"
-#include "Physics Engine/AABB.h"
-#include "Physics Engine/Circle.h"
-#include "Physics Engine/Collision.h"
+#include "Body.h"
+#include "BodyAABB.h"
+#include "BodyCircle.h"
+#include "PhysicsMaterial.h"
+#include "AABB.h"
+#include "Circle.h"
+#include "Collision.h"
 
 #endif /* defined(__Brickpong__Physics_Engine__) */
