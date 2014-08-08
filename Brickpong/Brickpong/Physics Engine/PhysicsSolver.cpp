123613e05e8af3d6d59dc0ca7328c7e29be2be53
Michał Piekarski <michal.piekarski@icloud.com>
2014-08-08 23:37:52 +0200

    Nowe parametry w klasie PhysicsSolver silnika fizycznego.
    - _frameStartTime - czas rozpoczęcia poprzedniej "klatki"
    - _bodyCount - ilość obiektów biorących udział w obliczeniach silnika fizycznego (nie można zmieniać bez zmiany _bodyList)
    - _bodyList - lista obiektów biorących udział w obliczeniach silnika fizycznego (tylko do zapisu)
    
    Dodane też odpowiednie metody do operowania tymi parametrami.

--- Brickpong/Brickpong/Physics Engine/PhysicsSolver.cpp
+++ Brickpong/Brickpong/Physics Engine/PhysicsSolver.cpp
@@ -1,16 +1,25 @@
 #include "PhysicsSolver.h"
+#include "Body.h"
 
 PhysicsSolver::PhysicsSolver() {
     _dt = 1.0f / 60.0f;
+    _frameStartTime = 0.0f;
     _gravity = glm::vec2(1.0f,0.0f);
+    _bodyCount = 0;
 }
 PhysicsSolver::PhysicsSolver(glm::vec2 gravity, float dt) {
     _dt = dt;
+    _frameStartTime = 0.0f;
     _gravity = gravity;
-
+}
+PhysicsSolver::PhysicsSolver(glm::vec2 gravity, float dt, int bodyCount, std::list<Body*> bodyList) {
+    _dt = dt;
+    _frameStartTime = 0.0f;
+    _gravity = gravity;
+    _bodyCount = bodyCount;
+    _bodyList = bodyList;
 }
 PhysicsSolver::~PhysicsSolver() {
-
 }
 float PhysicsSolver::getDeltaTime() {
     return _dt;
@@ -23,6 +32,19 @@ glm::vec2 PhysicsSolver::getGravity() {
 }
 void PhysicsSolver::setGravity(glm::vec2 gravity) {
     _gravity = gravity;
+}
+float PhysicsSolver::getFrameStartTime() {
+    return _frameStartTime;
+}
+void PhysicsSolver::setFrameStartTime(float frameStartTime) {
+    _frameStartTime = frameStartTime;
+}
+int PhysicsSolver::getBodyCount() {
+    return _bodyCount;
+}
+void PhysicsSolver::setBodyList(int bodyCount, std::list<Body*> bodyList) {
+    _bodyCount = bodyCount;
+    _bodyList = bodyList;
 }
 void PhysicsSolver::step() {
     //collide
--- Brickpong/Brickpong/Physics Engine/PhysicsSolver.h
+++ Brickpong/Brickpong/Physics Engine/PhysicsSolver.h
@@ -3,18 +3,28 @@
 
 #include <glm/glm.hpp>
 
+class Body;
+
 class PhysicsSolver {
 private:
     float _dt; // Delta time
+    float _frameStartTime;
     glm::vec2 _gravity;
+    int _bodyCount;
+    std::list<Body*> _bodyList;
 public:
     PhysicsSolver();
     PhysicsSolver(glm::vec2 gravity, float dt);
+    PhysicsSolver(glm::vec2 gravity, float dt, int bodyCount, std::list<Body*> bodyList);
     virtual ~PhysicsSolver();
     float getDeltaTime();
     void setDeltaTime(float dt);
     glm::vec2 getGravity();
     void setGravity(glm::vec2 gravity);
+    float getFrameStartTime();
+    void setFrameStartTime(float frameStartTime);
+    int getBodyCount();
+    void setBodyList(int bodyCount, std::list<Body*> bodyList);
     void step();
 };
 
