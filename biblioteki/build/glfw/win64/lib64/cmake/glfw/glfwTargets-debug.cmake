d3165d0672d6b1892de666185919df6de76d1eec
Michał Piekarski <michal.piekarski@icloud.com>
2014-08-17 23:57:30 +0200

    Biblioteki skompilowane dla Windows x64 (64-bitowy) dodane do repozytorium.
    Wszystkie biblioteki, są skompilowane na podstawie kodu pochodzącego z submodule (z wyjąkiem biblioteki glew).

---
+++ biblioteki/build/glfw/win64/lib64/cmake/glfw/glfwTargets-debug.cmake
@@ -0,0 +1,20 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "glfw" for configuration "Debug"
+set_property(TARGET glfw APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(glfw PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib64/glfw3dll.lib"
+  IMPORTED_LINK_INTERFACE_LIBRARIES_DEBUG "winmm;opengl32"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib64/glfw3.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS glfw )
+list(APPEND _IMPORT_CHECK_FILES_FOR_glfw "${_IMPORT_PREFIX}/lib64/glfw3dll.lib" "${_IMPORT_PREFIX}/lib64/glfw3.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
