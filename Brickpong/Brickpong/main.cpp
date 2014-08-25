5823a1ae250d54f3f5d4d597dd3301138b4413b7
Michał Piekarski <michal.piekarski@icloud.com>
2014-08-25 20:24:05 +0200

    Skorygowane macierze widoku i projekcji.
    Wyeliminowane zniekształcenia i naprawiona pozycja kamery na osi Z (co wyeliminowało odbicie współrzędnych w tej osi).

--- Brickpong/Brickpong/main.cpp
+++ Brickpong/Brickpong/main.cpp
@@ -326,8 +326,8 @@ int main(void) {
         GLuint MatrixID = shaderProgram->getUniformLoc("MVP");
 
         glm::mat4 Model = glm::mat4(1.0f);
-        glm::mat4 View = glm::lookAt(glm::vec3(0, 0, -20), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
-        glm::mat4 Projection = glm::ortho(-10.0f, 10.0f, -9.0f, 2.0f, -10.0f, 50.0f);
+        glm::mat4 View = glm::lookAt(glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        glm::mat4 Projection = glm::ortho(-8.0f, 8.0f, -8.0f, 1.0f, -1.0f, 1.0f); // Powinna być zachowana taka sama proporcja szerokość:wysokość jaką ma okno (16:9) aby uniknąć zniekształceń
 
         glm::mat4 tmpModel, tmpMVP;
 
